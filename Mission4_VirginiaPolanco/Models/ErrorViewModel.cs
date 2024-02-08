d9461adcd4381a7230a804fb068b35eab75da105
vicki polanco <vickyp812@gmail.com>
2024-02-08 01:22:46 -0700

    Add project files.

---
+++ Mission4_VirginiaPolanco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission4_VirginiaPolanco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission4_VirginiaPolanco/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿let resultValue = null; 
+function checkNumber(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the value of the input field
+    let fHours = parseFloat($("#hoursWanted").val());
+    let iFixedCost = parseInt($("#fixedCost").val());
+    $("#message").text("");
+
+    // Check if the number is less than 0
+    if (fHours < 0)
+    {
+        // Clear the input field & display error message
+        $("#hoursWanted").val("");
+        $("#message").text("The number of hours needs to be greater than 0");
+        $("#outputBox").hide(); 
+
+    }
+    else
+    {
+        resultValue = fHours * iFixedCost;
+
+        if (resultValue > 0) {
+            // Update the output box with the result
+            $("#result").text(resultValue);
+            $("#outputBox").show();
+        }
+        else
+        {
+            $("#outputBox").hide(); 
+        }
+    }
+}
+
+$("#button").click(checkNumber);
+
